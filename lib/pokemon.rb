89fbf31e2abac2a539668e4c1f301c6f9f343e1d
Jon Mackey <jon@jonmackey.party>
2019-01-25 21:26:00 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,9 +2,9 @@ class Pokemon
   @@all = []
   
   def initialize(id:, name:, type:, db:)
-    @id = id
-    @name = name
-    @type = type
+    @id = id:
+    @name = name:
+    @type = :
     @db = db 
     @@all << self
   end
